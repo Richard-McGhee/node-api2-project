18748928c0362bf3920fa8300ca90a6c410dfad2
richard-mcghee <richardjmcghee@yahoo.com>
2020-09-01 22:17:52 -0400

    got things up and running, get request to posts running as well as the post request

--- index.js
+++ index.js
@@ -6,7 +6,37 @@ const server = express()
 server.use(express.json())
 server.use("/api/posts", commentRouter)
 
+server.get("/", (req, res) => {
+    if(req){
+        res.status(200).json({ greeting: "We're Live"})
+    } else{
+        res.status(500).json({ errorMessage: "Something Broke"})
+    }
+})
 
+server.get("/api/posts", (req, res) => {
+    db.find()
+    .then(posts => res.status(200).json({ data: posts }))
+    .catch(err => {
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    })
+})
+
+server.post("/api/posts", (req, res) => {
+    const { title, contents } = req.body
+
+    if(title && contents){
+        db.insert(req.body)
+        .then(post => res.status(201).json({ data: post }))
+        .catch(err => {
+            res.status(500).json({ error: "There was an error while saving the post to the database" })
+        })
+    } else if(!title || !contents){
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else{
+        res.status(500).json({ error: "There was an error while saving the post to the database" })
+    }
+})
 
 const port = 666
 server.listen(port, () => console.log(`Server is up listening on Satan's port: ${port}`))
